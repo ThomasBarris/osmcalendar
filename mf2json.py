1d3f7e3cf34db90486b8e4c0aba1c65bb0fb6b5c
Thomas Barris <thomas.barris@googlemail.com>
2017-11-05 18:00:34 +0100

    bug fixes

--- mf2json.py
+++ mf2json.py
@@ -11,16 +11,17 @@ from geopy.geocoders import Nominatim      # geocoding lib from https://github.c
 from urllib.request import urlopen         # we need to read the html file directly to recognise BIG
 from time import sleep                     # Nominatim is very sensitive and we must wait between to requests
 from datetime import datetime              # for the timestamp in the output
+from bs4 import BeautifulSoup              # for stripping html tags from raw html for finding "big" and conference type
 
 #########################################################################
 # file and path holding the exceptions                                  #
-exception_file = 'excp.lst'                                             #
+exception_file = '/var/www/html/osmc/excp.lst'                      #
 #                                                                       #
 # file and path to save the json file with the results                  #
-result_json = '/var/www/html/calendar.json'                             #
+result_json = '/var/www/html/osmc/calendar.json'                             #
 #                                                                       #
 # file and path to store the unhandled exceptions                       #
-error_json  = '/var/www/html/error.json'                                #
+error_json  = '/var/www/html/osmc/error.json'                                #
 #                                                                       #
 # where to read the html with the Microformat2 coded calendar entries   #
 wiki_url = "http://wiki.openstreetmap.org/wiki/Current_events"          #
@@ -71,27 +72,37 @@ exception_l = [x.strip('\n') for x in exception_l]
 
 #iterate through the input json
 for each in formated_json:
-    # we will store the output data for a single line here
-    out_data = {}
+    # we will store the output data for a single line/event here
+    out_data_line = {}
     out_error_line = {}
+
+    # we don't now wether a date belongs to an event with errors or not, we will assign it to the appropraite dict later
+    prelim_out_data = {}
 
     ######################reading properties START and END################################
 
     #not all entries have dates
     try:
         print (each['properties']['start'][0], end=end_str)
-        out_data['start'] = each['properties']['start'][0]
+        prelim_out_data['start'] = each['properties']['start'][0]
     #if not, __-__-____
     except:
         print ('__-__-____', end=end_str)
-        out_data['start'] = ''
+        prelim_out_data['start'] = ''
     #same for end date
     try:
+        # the mf2py lib returns the end date +1...for whatever reason
+        # so copy the last two chars, convert to integer, reduce it by 1, 
+        # convert back to string with two decimals and copy it together
+        end_day_int = int( each['properties']['end'][0][-2:]) -1
+        end_day_string = '{0:02}'.format(end_day_int)
+        each['properties']['end'][0] = each['properties']['end'][0][:-2] + end_day_string
+
         print (each['properties']['end'][0], end=end_str)
-        out_data['end'] = each['properties']['end'][0]
+        prelim_out_data['end'] = each['properties']['end'][0]
     except:
         print ('__-__-____', end=end_str)
-        out_data['end'] = ''
+        prelim_out_data['end'] = ''
 
     ######################reading property name###########################################
 
@@ -124,7 +135,7 @@ for each in formated_json:
     # >>>> organize data fields for exceptions i.e. 4 or more data fields in property name
     ######################################################################################
 
-    ## if there are 4 items in the list originating from the 'names' field, we muist be careful
+    ## if there more than 3 items in the list originating from the 'names' field, we must be careful
     if k_length >= 4:
 
         #iterate through the exception list loaded from the file ealier in this script and compare the current entry
@@ -145,32 +156,35 @@ for each in formated_json:
     ## if it is a valid entry, print it and write the required values to the string
     if (excp == 0):
 
-       # >>>> write country, state, town and description
+        # assign the dates to the dict
+        out_data_line = prelim_out_data
+
+        # >>>> write country, state, town and description
 
         print ('Country : ', end='')
         print (kuddelmuddel_list[country].lstrip(), end = ' / ')
-        out_data['country'] = kuddelmuddel_list[country].lstrip()
+        out_data_line['country'] = kuddelmuddel_list[country].lstrip()
 
         if state !=99:
             print ('State : ', end='')
             print (kuddelmuddel_list[state].lstrip(), end = ' / ')
-            out_data['state'] = kuddelmuddel_list[state].lstrip()
+            out_data_line['state'] = kuddelmuddel_list[state].lstrip()
         else:
-            out_data['state'] = ''
+            out_data_line['state'] = ''
 
         print ('Town : ', end='')
         print (kuddelmuddel_list[town].lstrip(), end = ' / ')
-        out_data['town'] = kuddelmuddel_list[town].lstrip()
+        out_data_line['town'] = kuddelmuddel_list[town].lstrip()
 
         print (kuddelmuddel_list[desc].lstrip(), end= ' ***** ')
-        out_data['description'] = kuddelmuddel_list[desc]
+        out_data_line['description'] = kuddelmuddel_list[desc]
 
         # >>>> optional geocoding
 
         # if geocoding was set true in settings, add lat/lon if available
         if (geocoding):
             # get location for town/country
-            location = geolocator.geocode(  out_data['country']+out_data['town'])
+            location = geolocator.geocode(  out_data_line['country']+out_data_line['town'])
 
             # wait 2 seconds to allow Nominatim to calm down
             sleep(2)
@@ -178,11 +192,11 @@ for each in formated_json:
             # not every value can be translated to an address
             try:
                 print((location.latitude, location.longitude), end='')
-                out_data['latitude'] = location.latitude
-                out_data['longitude'] = location.longitude
+                out_data_line['latitude'] = location.latitude
+                out_data_line['longitude'] = location.longitude
             except:
-                out_data['latitude'] = ''
-                out_data['longitude'] = ''
+                out_data_line['latitude'] = ''
+                out_data_line['longitude'] = ''
 
         # >>> processing raw html 
 
@@ -190,111 +204,109 @@ for each in formated_json:
         big_code = '<big>'
 
         ## default is that it is not a big event
-        out_data['Big'] = ''
+        out_data_line['Big'] = ''
 
-        ## unfortunatelly the event type is 2 lines before the event description so we need to remember 2 lines history
+        ## unfortunatelly the event type is 2 lines before the event description in the html so we need to remember 2 lines history
         h_line = ''
         prev_h_line = ''
         prev_prev_h_line=''
 
-        ## iterate through list with raw html content
+        ## iterate through list with raw html content and remember the last 2 lines
         for html_line in wiki_html_list:
             prev_prev_h_line = prev_h_line
             prev_h_line = h_line 
             h_line = html_line.decode("utf8")
 
-            ################ if event description found in raw html code
-            if ( h_line.find(kuddelmuddel_list[desc]) >= 0 ):
+            # strip html tags from html raw line, see https://stackoverflow.com/questions/9662346/python-code-to-remove-html-tags-from-a-string
+            html_cleantext = BeautifulSoup(h_line, "html5lib").text
 
-                ##############.....and also the html-code for a big event is found, set "Big":"True"
+            ################ if event description found in raw html code lines
+            # .....striped by html tags as the event description in html can be
+            # "State of the Map 2018</a></b></big> (international conference)" 
+            # which is not equal with Microformat description/evenmt name "State of the Map 2018 (international conference)"
+            if ( html_cleantext.find(kuddelmuddel_list[desc]) >= 0 ):
+
+                ##############.....and also the html-tag for a big event is found, set "Big":"True"
                 if ( h_line.find(big_code) >= 0 ):
-                    out_data['Big'] = 'True'
-                    print ('BIG', end='')
+                    out_data_line['Big'] = 'True'
+                    print ('BIG', end=' ***** ')
+
 
                 ##############...find the event type in raw html, 2 lines before we found the description and set the event type
                 if   ( prev_prev_h_line.find('class="p-category" title="Mapping party"') >= 0 ):
-                    out_data['EventType'] = 'Mapping Party' #for mapping parties (with surveying) or mapathons (without)
+                    out_data_line['EventType'] = 'Mapping Party' #for mapping parties (with surveying) or mapathons (without)
 
                 elif ( prev_prev_h_line.find('class="p-category" title="Social"') >= 0 ):
-                    out_data['EventType'] = 'Social'         #for informal meetings, or just to have a beer with fellow OSM addicts.
+                    out_data_line['EventType'] = 'Social'         #for informal meetings, or just to have a beer with fellow OSM addicts.
 
                 elif ( prev_prev_h_line.find('class="p-category" title="Meeting"') >= 0 ):
-                    out_data['EventType'] = 'Meeting'         #for meetings at a face-to-face location or virtual
+                    out_data_line['EventType'] = 'Meeting'         #for meetings at a face-to-face location or virtual
 
                 elif ( prev_prev_h_line.find('class="p-category" title="Speaking"') >= 0 ):
-                    out_data['EventType'] = 'Speaking'         #see below
+                    out_data_line['EventType'] = 'Speaking'         #see below
 
                 elif ( prev_prev_h_line.find('{{Cal|talk}}') >= 0 ): #?????????????????????????
-                    out_data['EventType'] = 'Speaking'         #for talks/speaking events with individual or small number of talks on OSM
+                    out_data_line['EventType'] = 'Speaking'         #for talks/speaking events with individual or small number of talks on OSM
 
                 elif ( prev_prev_h_line.find('class="p-category" title="Conference"') >= 0 ):
-                    out_data['EventType'] = 'Conference'        #for conferences with several talks/panels/workshops on OSM
+                    out_data_line['EventType'] = 'Conference'        #for conferences with several talks/panels/workshops on OSM
 
                 elif ( prev_prev_h_line.find('class="p-category" title="Pizza') >= 0 ):
-                    out_data['EventType'] = 'Pizza' #for hack-a-thons/mapathons involving pizza (or eating other meals, e.g. pasta-parties or brunches)
+                    out_data_line['EventType'] = 'Pizza' #for hack-a-thons/mapathons involving pizza (or eating other meals, e.g. pasta-parties or brunches)
 
                 elif ( prev_prev_h_line.find('class="p-category" title="IRC"') >= 0 ):
-                    out_data['EventType'] = 'IRC' 	#for IRC meetings about OSM.
+                    out_data_line['EventType'] = 'IRC' 	#for IRC meetings about OSM.
 
                 elif ( prev_prev_h_line.find('class="p-category" title="TV') >= 0 ):
-                    out_data['EventType'] = 'TV' 	#for TV/press appearances. Turn on your TV that day!
+                    out_data_line['EventType'] = 'TV' 	#for TV/press appearances. Turn on your TV that day!
 
                 elif ( prev_prev_h_line.find('class="p-category" title="Radio/Podcast"') >= 0 ):
-                    out_data['EventType'] = 'Podcast' #for radio/podcast recordings
+                    out_data_line['EventType'] = 'Podcast' #for radio/podcast recordings
 
                 elif ( prev_prev_h_line.find('class="p-category" title="Information') >= 0 ):
-                    out_data['EventType'] = 'Info' #for important dates e.g OSMF election deadlines
+                    out_data_line['EventType'] = 'Info' #for important dates e.g OSMF election deadlines
 
                 elif ( prev_prev_h_line.find('class="p-category" title="Miscellaneous"') >= 0 ):
-                    out_data['EventType'] = 'Misc' #whatever
+                    out_data_line['EventType'] = 'Misc' #whatever
 
                 else:
-                    out_data['EventType'] = '' #nothing recognised
+                    out_data_line['EventType'] = '' #nothing recognised
+        print (out_data_line['EventType'], end='' )
+
+
 
         # end of line/entry
         print()
 
         ## add the entry i.e. python dict to the list that will later be exported as json
-        out_array.append(out_data)
+        out_array.append(out_data_line)
 
     else:
-        ## special exception for our french friends
-#        try:
-#            if (kuddelmuddel_list[3].lstrip() == 'France') and (kuddelmuddel_list[2].lstrip() == 'tous concernés ! Digne-les-Bains'):
-#                print ('Country : ', end='')
-#                print (kuddelmuddel_list[3].lstrip(), end = ' / ')
-#                out_data['country'] = kuddelmuddel_list[3]
-#
-#                print ('Town : ', end='')
-#                print ('Digne-les-Bains', end = ' / ')
-#                out_data['town'] = 'Digne-les-Bains'
-#
-#                print (kuddelmuddel_list[0].lstrip())
-#                out_data['description'] = kuddelmuddel_list[0].lstrip()
-#                out_array.append(out_data)
-#                excp = 0
-#        except:
-#            pass
+        # the entries could not be read so write such entries to an error file
 
-        ## excp is not null and it is not the french exceptiuon....add it this as an error entry
-        if excp !=0:
-            print ('EXC', end = ': ')
-            print (k_length, end = ' ')
-            print ('data fields', end = ' : ')
-            for x in range(0, 4):
-                try:
-                    print (kuddelmuddel_list[x].lstrip(), end = '##')
-                    out_error_line[str(x)] = kuddelmuddel_list[x].lstrip()
-                except:
-                    pass
-            print()
+        # add the start date and the edn date to the error dict
+        out_error_line = prelim_out_data
 
-            #add the error entry to the list that will be later exported as error.json
-            out_error.append(out_error_line)
+        # mark it with "EXC" in the console and add the data to the json with the faulty entires
+        print ('EXC', end = ': ')
+        print (k_length, end = ' ')
+        print ('data fields', end = ' : ')
+        for x in range(0,6):
+            try:
+                print (kuddelmuddel_list[x].lstrip(), end = '##')
+                out_error_line[str(x)] = kuddelmuddel_list[x].lstrip()
+            except:
+                pass
+        print()
+
+        #add the error entry to the list that will be later exported as error.json or similar name as defined at the beginning
+        out_error.append(out_error_line)
 
 # create the frame around the good results with timestamp and meta data
 dt = datetime.now()
 timestamp = dt.strftime("%A, %d. %B %Y %I:%M%p")
+print ('>>>>>>>>>>>>>>>>>>>>> report generated :', end='')
+print (timestamp)
 out_json = { "version": osmc_version,
              "generator": "osmcalender",
              "time": timestamp,
